005b760eccd01b72c8d89976481d2c23fd37934a
Igone van_der_Kley <igone@vdkley.nl>
2024-05-08 13:26:46 +0200

    New navbar links added to partial

--- resources/views/partials/_navbar.blade.php
+++ resources/views/partials/_navbar.blade.php
@@ -6,15 +6,26 @@
         <a class="navbar-brand" href="/"><img src="/img/jamin-logo.png" alt="logo"></a>
         <ul class="navbar-nav">
             <li class="nav-item">
+                <a class="nav-link" href="/">Dashboard</a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link" href="/overzicht-magazijn">Magazijn overzicht</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/overzicht-leveranciers">Leveranciers</a>
             </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/voeg-product">Nieuw product</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/voeg-leverancier">Nieuwe leverancier</a>
+            </li>
+
         </ul>
     </div>
 </nav>
 
 <style>
-  
+
 </style>
