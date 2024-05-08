35445928c830d96ce7f1e94ce6023ec27c92c573
Igone van_der_Kley <igone@vdkley.nl>
2024-05-08 13:29:48 +0200

    Icons added to homepage for functionalites, e.g new delivery, new distributor/vendor, new product

--- resources/views/welcome-jamin.blade.php
+++ resources/views/welcome-jamin.blade.php
@@ -19,8 +19,26 @@
                 <div class="welcome-card">
                     <div>
                         <h1>Welcome bij Jamin</h1>
-                        <a href="overzicht-magazijn">Magazijn overzicht</a>
-                        <a href="overzicht-leveranciers">Leveranciers</a>
+
+                        <center>
+                            <h4>Iets nieuws toevoegen?</h4>
+                        </center>
+                        <ul class="function-icons">
+                            <a href="voeg-leverancier">
+                                <li><img src="img/distributor.png" alt="">Leverancier</li>
+                            </a>
+                            <a href="voeg-product">
+                                <li><img src="img/candy.png" alt="">Product</li>
+                            </a>
+                            <a href="overzicht-leveranciers">
+                                <li><img src="img/delivery.png" alt="">Levering</li>
+                            </a>
+                        </ul>
+
+                        <div class="reads">
+                            <a href="overzicht-magazijn">Magazijn overzicht</a>
+                            <a href="overzicht-leveranciers">Leveranciers</a>
+                        </div>
                     </div>
                 </div>
             </div>
