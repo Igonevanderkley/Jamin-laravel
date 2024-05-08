06ff4e420d3022b81e9d8eb6666120269e8c2779
Igone van_der_Kley <igone@vdkley.nl>
2024-05-08 09:46:48 +0200

    Added update functionality for products

--- app/Http/Controllers/MagazijnController.php
+++ app/Http/Controllers/MagazijnController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+
+use App\Models\Magazijn;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
@@ -15,5 +17,55 @@ class MagazijnController extends Controller
         return view('overzicht-magazijn', [
             'magazijnItems' => $magazijnItems
         ]);
+    }
+    
+
+    public function wijzigIndex($itemId)
+    {
+        $productData = Product::where('id', $itemId)->get();
+        $magazijnData = Magazijn::where('productId', $itemId)->get();
+
+        return view('wijzig-product', [
+            'productData' => $productData,
+            'magazijnData' => $magazijnData
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $validatedData = $request->validate([
+            'naam' => 'required|string',
+            'barcode' => 'required|numeric',
+            'verpakkingsEenheid' => 'required',
+            'aantalAanwezig' => 'integer'
+        ], [
+            'naam.required' => 'Naam is verplicht.',
+            'barcode.required' => 'Barcode is verplicht.',
+            'barcode.numeric' => 'Barcode moet valide getal zijn.',
+            'verpakkingsEenheid.required' => 'Verpakkings eenheid is verplicht.',
+        ]);
+
+        $product = Product::find($request->id);
+        $product->naam = $validatedData['naam'];
+        $product->barcode = $validatedData['barcode'];
+        $product->save();
+
+        $magazijnProduct = Magazijn::where('productId', $request->id)->first();
+        $magazijnProduct->verpakkingsEenheid = $validatedData['verpakkingsEenheid'];
+        $magazijnProduct->aantalAanwezig = $validatedData['aantalAanwezig'];
+        $magazijnProduct->save();
+
+        return redirect()->route('overzicht-magazijn');
+    }
+
+    public function destroy($itemId)
+    {
+        $product = Product::find($itemId);
+        $product->delete();
+
+        $magazijnProduct = Magazijn::where('productId', $itemId)->first();
+        $magazijnProduct->delete();
+
+        return redirect()->route('overzicht-magazijn');
     }
 }
